6cd1e8f33ce6399c8436a5d07345c816f635a310
Lunar Starstrum <lunar@forsaken-borders.net>
2023-08-22 01:55:28 -0500

    Slightly clean this up

--- src/AssemblyProviders/NugetAssemblyProvider.cs
+++ src/AssemblyProviders/NugetAssemblyProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -34,48 +35,54 @@ namespace OoLunar.DocBot.AssemblyProviders
 
         public async ValueTask<IEnumerable<Assembly>> GetAssembliesAsync()
         {
-            string[]? packages = _configuration.GetSection("Nuget:Packages").Get<string[]>();
-            if (packages is null || packages.Length == 0)
+            IEnumerable<IConfigurationSection> packages = _configuration.GetSection("nuget:packages").GetChildren();
+            if (!packages.Any())
             {
-                return Array.Empty<Assembly>();
+                _logger.LogWarning("No packages specified in configuration");
+                return Enumerable.Empty<Assembly>();
             }
 
             string path = _configuration.GetValue("Nuget:Path", "packages")!;
             List<string> installedPackages = new();
             FindPackageByIdResource resource = await _repository.GetResourceAsync<FindPackageByIdResource>();
-            foreach (string package in packages)
+            foreach (IConfigurationSection package in packages)
             {
-                IEnumerable<NuGetVersion> versions = await resource.GetAllVersionsAsync(package, _repositoryCache, NugetNullLogger.Instance, default);
-                if (!versions.Any())
+                // Ensure the package has a valid version and exists
+                if (!NuGetVersion.TryParse(package.Value, out NuGetVersion? packageVersion) || packageVersion is null)
                 {
-                    _logger.LogWarning("Package {Package} has no versions", package);
+                    _logger.LogError("Package \"{Package}\" has an invalid version: {Version}", package.Key, package.Value);
+                    continue;
+                }
+                else if (!await resource.DoesPackageExistAsync(package.Key, packageVersion, _repositoryCache, NugetNullLogger.Instance, default))
+                {
+                    _logger.LogError("Package \"{Package}\" with version \"{Version}\" does not exist.", package.Key, packageVersion);
                     continue;
                 }
 
-                NuGetVersion version = versions.LastOrDefault(version => !version.IsLegacyVersion) ?? versions.LastOrDefault(version => version.IsPrerelease) ?? versions.Last();
                 MemoryStream packageStream = new();
-                await resource.CopyNupkgToStreamAsync(package, version, packageStream, _repositoryCache, NugetNullLogger.Instance, default);
-                _logger.LogInformation("Downloaded package {Package} {Version}", package, version);
+                await resource.CopyNupkgToStreamAsync(package.Key, packageVersion, packageStream, _repositoryCache, NugetNullLogger.Instance, default);
+                _logger.LogInformation("Downloaded package \"{Package}\" with version \"{Version}\"", package.Key, packageVersion);
 
                 using PackageArchiveReader packageReader = new(packageStream);
                 IEnumerable<FrameworkSpecificGroup> frameworkGroups = packageReader.GetReferenceItems();
                 if (!frameworkGroups.Any())
                 {
-                    _logger.LogWarning("Package {Package} {Version} has no reference items", package, version);
+                    _logger.LogError("Package \"{Package}\" with version \"{Version}\" has no reference items", package, packageVersion);
                     continue;
                 }
 
                 FrameworkSpecificGroup? frameworkGroup = frameworkGroups.FirstOrDefault(framework => !framework.HasEmptyFolder);
                 if (frameworkGroup is null)
                 {
-                    _logger.LogWarning("Package {Package} {Version} has no non-empty framework groups", package, version);
+                    _logger.LogError("Package \"{Package}\" with version \"{Version}\" has no non-empty framework groups", package, packageVersion);
                     continue;
                 }
 
-                IEnumerable<string> selectedItems = frameworkGroup.Items.Where(item => item.EndsWith(".dll") || item.EndsWith(".xml"));
+                // Search for main dll and xml files
+                IEnumerable<string> selectedItems = frameworkGroup.Items.Where(item => item.EndsWith(".dll", true, CultureInfo.InvariantCulture) || item.EndsWith(".xml", true, CultureInfo.InvariantCulture));
                 if (!selectedItems.Any())
                 {
-                    _logger.LogWarning("Package {Package} {Version} has no dll or xml files", package, version);
+                    _logger.LogError("Package \"{Package}\" with version \"{Version}\" has no dll or xml files", package, packageVersion);
                     continue;
                 }
 
@@ -84,12 +91,11 @@ namespace OoLunar.DocBot.AssemblyProviders
                     string packagePath = Path.Combine(path, item);
                     packageReader.ExtractFile(item, packagePath, NugetNullLogger.Instance);
 
-                    if (packagePath.EndsWith(".dll"))
+                    if (packagePath.EndsWith(".dll", true, CultureInfo.InvariantCulture))
                     {
                         installedPackages.Add(packagePath);
                     }
                 }
-
             }
 
             return installedPackages.Count == 0
