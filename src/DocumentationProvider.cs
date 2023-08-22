acb5d9e21fb573ec68781b4f4993db77d5de0d15
Lunar Starstrum <lunar@forsaken-borders.net>
2023-08-22 14:41:34 -0500

    Inline ParseMembers

--- src/DocumentationProvider.cs
+++ src/DocumentationProvider.cs
@@ -6,7 +6,6 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
-using DSharpPlus.Entities;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Namotion.Reflection;
@@ -49,7 +48,7 @@ namespace OoLunar.DocBot
             }
 
             Dictionary<Ulid, DocumentationMember> members = new();
-            foreach (DocumentationMember member in ParseMembers(GetMembers(assemblies)))
+            foreach (DocumentationMember member in GetMembers(assemblies))
             {
                 _logger.LogTrace("Loaded: {MemberName}", member.DisplayName);
                 members[member.Id] = member;
@@ -60,9 +59,9 @@ namespace OoLunar.DocBot
             return;
         }
 
-        private static ConcurrentQueue<MemberInfo> GetMembers(IEnumerable<Assembly> assemblies)
+        private static ConcurrentQueue<DocumentationMember> GetMembers(IEnumerable<Assembly> assemblies)
         {
-            ConcurrentQueue<MemberInfo> members = new();
+            ConcurrentQueue<DocumentationMember> members = new();
             Parallel.ForEach(assemblies, assembly =>
                 Parallel.ForEach(assembly.GetExportedTypes(), type =>
                     Parallel.ForEach(type.GetMembers(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly), member =>
@@ -72,63 +71,53 @@ namespace OoLunar.DocBot
                             return;
                         }
 
-                        // We can't lock a list and do AddRange because we still have to filter out special names.
-                        members.Enqueue(member);
+                        StringBuilder stringBuilder = new("## ");
+                        string summary = member.GetXmlDocsSummary(_defaultXmlDocsOptions);
+                        string? remarks = member.GetXmlDocsRemarks(_defaultXmlDocsOptions);
+                        if (string.IsNullOrWhiteSpace(summary))
+                        {
+                            summary = "No summary provided.";
+                        }
+
+                        if (string.IsNullOrWhiteSpace(remarks))
+                        {
+                            remarks = null;
+                        }
+
+                        Uri? source = null;
+                        if (source is null)
+                        {
+                            stringBuilder.Append(member.DeclaringType is null ? "<runtime generated>" : member.DeclaringType.FullName);
+                            stringBuilder.Append('.');
+                            stringBuilder.Append(member.Name);
+                        }
+                        else
+                        {
+                            stringBuilder.Append('[');
+                            stringBuilder.Append(member.DeclaringType is null ? "<runtime generated>" : member.DeclaringType.FullName);
+                            stringBuilder.Append('.');
+                            stringBuilder.Append(member.Name);
+                            stringBuilder.Append("](");
+                            stringBuilder.Append(source);
+                            stringBuilder.Append(')');
+                        }
+
+                        stringBuilder.AppendLine();
+                        stringBuilder.AppendLine("### Summary");
+                        stringBuilder.AppendLine(summary);
+                        if (remarks is not null)
+                        {
+                            stringBuilder.AppendLine("### Remarks");
+                            stringBuilder.AppendLine(remarks);
+                        }
+
+                        stringBuilder.AppendLine("### Declaration");
+                        stringBuilder.AppendLine(Formatter.BlockCode(member.GetDeclarationSyntax(), "cs"));
+
+                        members.Enqueue(new DocumentationMember(Ulid.NewUlid(), member.GetFullName(), stringBuilder.ToString().TrimLength(2048)));
                     })));
 
             return members;
-        }
-
-        private static IEnumerable<DocumentationMember> ParseMembers(IEnumerable<MemberInfo> members)
-        {
-            DiscordEmbedBuilder baseEmbedBuilder = new() { Color = new DiscordColor(0x6b73db) };
-            foreach (MemberInfo member in members)
-            {
-                StringBuilder stringBuilder = new("## ");
-                string summary = member.GetXmlDocsSummary(_defaultXmlDocsOptions);
-                string? remarks = member.GetXmlDocsRemarks(_defaultXmlDocsOptions);
-                if (string.IsNullOrWhiteSpace(summary))
-                {
-                    summary = "No summary provided.";
-                }
-
-                if (string.IsNullOrWhiteSpace(remarks))
-                {
-                    remarks = null;
-                }
-
-                Uri? source = null;
-                if (source is null)
-                {
-                    stringBuilder.Append(member.DeclaringType is null ? "<runtime generated>" : member.DeclaringType.FullName);
-                    stringBuilder.Append('.');
-                    stringBuilder.Append(member.Name);
-                }
-                else
-                {
-                    stringBuilder.Append('[');
-                    stringBuilder.Append(member.DeclaringType is null ? "<runtime generated>" : member.DeclaringType.FullName);
-                    stringBuilder.Append('.');
-                    stringBuilder.Append(member.Name);
-                    stringBuilder.Append("](");
-                    stringBuilder.Append(source);
-                    stringBuilder.Append(')');
-                }
-
-                stringBuilder.AppendLine();
-                stringBuilder.AppendLine("### Summary");
-                stringBuilder.AppendLine(summary);
-                if (remarks is not null)
-                {
-                    stringBuilder.AppendLine("### Remarks");
-                    stringBuilder.AppendLine(remarks);
-                }
-
-                stringBuilder.AppendLine("### Declaration");
-                stringBuilder.AppendLine(Formatter.BlockCode(member.GetDeclarationSyntax(), "cs"));
-
-                yield return new DocumentationMember(Ulid.NewUlid(), member.GetFullName(), stringBuilder.ToString().TrimLength(2048));
-            }
         }
     }
 }
