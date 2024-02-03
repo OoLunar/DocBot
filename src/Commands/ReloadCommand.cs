3de3d67ab0a33516e2476d86e3a3210c2ec2409d
Lunar Starstrum <lunar@forsaken-borders.net>
2024-02-02 22:32:10 -0600

    Update deps; Add reload command

---
+++ src/Commands/ReloadCommand.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Commands.ContextChecks;
+using DSharpPlus.Commands.Processors.SlashCommands.Attributes;
+using DSharpPlus.Commands.Processors.TextCommands.Attributes;
+using DSharpPlus.Commands.Trees;
+using DSharpPlus.Commands.Trees.Attributes;
+
+namespace OoLunar.DocBot.Commands
+{
+    public sealed class ReloadCommand
+    {
+        private readonly DocumentationProvider _documentationProvider;
+
+        public ReloadCommand(DocumentationProvider documentationProvider) => _documentationProvider = documentationProvider ?? throw new ArgumentNullException(nameof(documentationProvider));
+
+        [Command("reload"), TextAlias("restart"), Description("Reloads the bot documentation"), RequireOwner, SlashCommandTypes(ApplicationCommandType.SlashCommand)]
+        public async ValueTask ExecuteAsync(CommandContext context)
+        {
+            await context.DeferResponseAsync();
+            await _documentationProvider.ReloadAsync();
+            await context.RespondAsync("Documentation reloaded.");
+        }
+    }
+}
